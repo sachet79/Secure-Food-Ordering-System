1dd56e545a48c3132677967ca00f3e75cadf3023
sachet79 <sachet79@gmail.com>
2023-04-28 00:41:27 +0100

    heroku commit

--- Foods_Ordering/settings.py
+++ Foods_Ordering/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from pathlib import Path
+import django_heroku    
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,7 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR, "static/")
+STATIC_ROOT= os.path.join(BASE_DIR, "staticfiles/")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"))
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -157,5 +160,5 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE=True
 
 SESSION_TIMEOUT_REDIRECT = 'http://127.0.0.1:3002'
 
-
+django_heroku.settings(locals())
 
