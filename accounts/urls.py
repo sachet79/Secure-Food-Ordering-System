fae2b6a6de608bdd5c8302e6bdd1f4c9a57616ff
sachet79 <sachet79@gmail.com>
2023-04-30 14:37:15 +0100

    commit12

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.conf import settings
+from django.views.static import serve
 from . import views
 
 app_name = 'accounts'
@@ -7,4 +9,5 @@ urlpatterns = [
     path('signup/', views.signup_view, name="signup"),
     path('login/', views.login_view, name="login"),
     path('logout/', views.logout_view, name="logout"),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 ]
